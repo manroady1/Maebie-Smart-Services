e087b2b7e208a813980b93ed6b944bbac75dce58
manroady1 <gmanik1212@gmail.com>
2023-04-06 23:00:59 +0530

    Complete Homeservices Project

---
+++ Customer/android/app/src/main/kotlin/com/smartersvision/home_services/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.smarthomeservicesmaebie.maebieUser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Provider/android/app/src/main/kotlin/com/smartersvision/home_services_provider/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.smarthomeservicesmaebie.maebieProvider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
